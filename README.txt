f853319b7c75d178a1daec3680f041d766565513
Uyamikun <nabil.nabighah@gmail.com>
2021-01-27 10:58:06 +0700

    final commit

---
+++ README.txt
@@ -0,0 +1,35 @@
+***IMPORTANT!***
+TEST FILE DAN MAIN.PY HARUS 1 FOLDER
+
+Deskripsi :
+Program untuk mencari solusi cryptarithmetic dengan Brute Foce
+
+1. Program menjalankan hanya dari file
+2. jika ingin membuat file test tolong gunakan tanda (-) minus untuk garisnya dan tanda (+) berada di operand sebelah kanan (lihat contoh)
+3. gunakan spasi untuk memisahkan antara satuan,puluhan,ratusan,...
+
+CONTOH
+  SOAL+
+------
+BEGINI
+
+
+*************************************************
+Cara menggunakan program
+
+Linux :
+1. Extract Tucil1_13519168.zip
+2. Copy file test case yang ingin dibuka ke folder yang berisi main.py
+3. Buka terminal dan masuk ke path folder yang berisi main.py
+4. Tulis "python3 main.py" tanpa tanda kutip
+
+Windows :
+1. Extract Tucil1_13519168.zip
+2. Copy file test case yang ingin dibuka ke folder bin
+3. Buka "main.exe" di folder bin
+
+
+*************************************************
+Author 
+Created By :
+Nabil Nabighah - 13519168
--- src/main.py
+++ src/main.py
@@ -1,16 +1,6 @@
 from itertools import chain
 import time
 
-#open and read file
-f = open("test.txt", "r")
-isi = f.readlines()
-
-#add to a list (alphabet only)
-arrAwal = []
-for i in range(len(isi)):
-    arrAwal.append(isi[i].rstrip('\n').rstrip('+'))
-del arrAwal[len(arrAwal)-2]
-
 #stoc function from array of string
 def stoc(array):
     res = list(chain.from_iterable(array))
@@ -24,37 +14,63 @@ def delDupc(array):
             tmparr.append(i)
     return tmparr
 
-
+#mfungsi memasukkan opperand ke array operand
 def insopp(arrayopp,arrayawal):
     for i in range(len(arrayawal)-1):
         arrayopp.append(arrayawal[i])
 
-def arrangka(count):
-    tempoarr = []
-    for i in range(count):
-        tempoarr.append(i)
-    #[0,1,2,3,...,(len(arrayunik))]
-    return tempoarr
+#prosedur permutasi (bad code banget)
+def permutasi(cobainaja,ArrPerm):
+    if cobainaja <= 10:
+        for i in range(10):
+            for j in range(10):
+                if i != j :
+                    if cobainaja != 2:
+                        for k in range(10):
+                            if k != i and k != j :
+                                if cobainaja != 3:
+                                    for l in range(10):
+                                        if l != i and l != j and l !=k:
+                                            if cobainaja !=4:
+                                                for m in range(10):
+                                                    if m != i and m !=j and m != k  and m != l:
+                                                        if cobainaja !=5:
+                                                            for n in range(10):
+                                                                if n != i and n != j and n!= k and n != l and n != m:
+                                                                    if cobainaja != 6:
+                                                                        for o in range(10):
+                                                                            if o != i and o != j and o != k and o != l and o != m and o != n :
+                                                                                if cobainaja != 7:
+                                                                                    for p in range(10):
+                                                                                        if p != i and p != j and p != k and p != l and p != m and p != n and p != o :
+                                                                                            if cobainaja != 8:
+                                                                                                for q in range(10):
+                                                                                                    if q != i and q != j and q != k and q != l and q != m and q != n and q != o and q != p:
+                                                                                                        if cobainaja != 9:
+                                                                                                            for r in range(10):
+                                                                                                                if r != i and r != j and r != k and r != l and r != m and r != n and r != o and r !=p and r != q :
+                                                                                                                    ArrPerm += [[i,j,k,l,m,n,o,p,q,r]]
+                                                                                                        else:
+                                                                                                            ArrPerm += [[i,j,k,l,m,n,o,p,q]]
 
-def permutasi(listangka): 
-    # isEmpty
-    if len(listangka) == 0: 
-        return [] 
-  
-    # isOneElmt
-    if len(listangka) == 1: 
-        return [listangka] 
-    
-    # len(listangka) > 1
-    temparray = []
+                                                                                            else:#8
+                                                                                                ArrPerm += [[i,j,k,l,m,n,o,p]]
+                                                                                else:#7
+                                                                                    ArrPerm += [[i,j,k,l,m,n,o]]
+                                                                    else:#6
+                                                                        ArrPerm += [[i,j,k,l,m,n]]
+                                                        else:#5
+                                                            ArrPerm += [[i,j,k,l,m]]
+                                            else:#4
+                                                ArrPerm += [[i,j,k,l]]
+                                else:#3
+                                    ArrPerm += [[i,j,k]]
+                    else: #2
+                        ArrPerm += [[i,j]]
+    else:#hadle kasus >10
+        print("Karakter unik terlalu banyak (>10)")
 
-    for i in range(len(listangka)): 
-       element = listangka[i] 
-       sisa = listangka[:i] + listangka[i+1:] 
-       for perm in permutasi(sisa): 
-           temparray.append([element] + perm) 
-    return temparray 
-
+#fungsi mencari index character di array char unik
 def findcidx(char,arrayunik):
     found = False
     i = 0
@@ -63,86 +79,132 @@ def findcidx(char,arrayunik):
             found = True
         else:
             i += 1
-
     return i       
 
+# prosedur mencari solusi
+def isEqual(opperand,result,unik,ArrPerm,solusi,countfail):
+    tambah = 0
+    anssum = 0
+    firstcans = result[0][0]
+    firstc = []
+    for i in range(len(opperand)):
+        for c in range(len(opperand[i])):
+            if c == 0 :
+                firstc.append(opperand[i][c])
+            tambah += ArrPerm[findcidx(opperand[i][c],unik)]*10**(len(opperand[i])-c-1)
+
+    for c in range(len(result[0])):
+        anssum += ArrPerm[findcidx(result[0][c],unik)]*10**(len(result[0])-c-1)
+    
+
+    if tambah == anssum :
+        firstintans = ArrPerm[findcidx(result[0][0],unik)]
+        firstint = []
+        #handle huruf depan = 0
+        for i in range(len(firstc)):
+            firstint.append(ArrPerm[findcidx(firstc[i],unik)])
+        if checkzero(firstint) == False :
+            if firstintans != 0 :
+                solusi.append(ArrPerm)
+    else:
+        countfail[0] += 1 
+        
+def checkzero(array):
+    found = False
+    i = 0
+    while not(found) and i < len(array):
+        if array[i] == 0:
+            found = True
+        else:
+            i += 1
+    
+    if found == True :
+        return True
+    else :
+        return False
+
+#open and read file
+filename = input("Tuliskan nama file dengan extensinya (contoh.txt) :")
+print("Mohon tunggu sedang diproses...\n")
+f = open(filename, "r")
+isi = f.readlines()
+
+#add to a list (alphabet only)
+start_time = time.time()
+arrAwal = []
+#arrAwalv2 = []
+for i in range(len(isi)):
+#    arrAwalv2.append(isi[i].rstrip('\n'))
+    arrAwal.append(isi[i].rstrip('\n').rstrip('+').replace(" ",""))
+savesign = []
+savesign.append(arrAwal[len(arrAwal)-2])
+del arrAwal[len(arrAwal)-2]
+
+
 #global variable &  inisiasi array
-
-
+ArrPerm = []
 arrNumber = []
 opperand = []
 result = []
 solusi = []
+countfail = [0]
 #element result diisi dengan result di file
 result.append(arrAwal[len(arrAwal) -1])
-#operand diisi ke list ooperand 
+
+#operand diisi ke array ooperand 
 insopp(opperand,arrAwal)
 
 #make array of huruf unik
 huruf = stoc(arrAwal)
 unik = delDupc(huruf)
 
-# print(opperand)
-# print(result)
-# print(unik)
-
-
-#cari solusi
-def isEqual(opperand,result,unik,ArrPerm,solusi):
-    tambah = 0
-    anssum = 0
-    #[SEND,MORE]
-    for i in range(len(opperand)):
-        for c in range(len(opperand[i])):
-            # print(sum)
-            tambah += ArrPerm[findcidx(opperand[i][c],unik)]*10**(len(opperand[i])-c-1)
-           # print(tambah,ArrPerm,opperand[i])
-
-    for c in range(len(result[0])):
-        # print(anssum)
-        anssum += ArrPerm[findcidx(result[0][c],unik)]*10**(len(result[0])-c-1)
-        # print(anssum,ArrPerm,result[0])
-    
-    # if tambah != 10652 and anssum == 10652 :
-    #     print(ArrPerm)
-    #     print(tambah)
-    #     print(anssum)
-    # if tambah == 10652 and anssum != 10652 :
-    #     print(unik)
-    #     print(ArrPerm)
-    #     print(tambah)
-        # print(anssum)
-    if tambah == anssum :
-        solusi.append(ArrPerm)
-        #sprint(solusi)
-
 # main driver
-#isi array angka sesuai panjang isi array huruf unik
-arrNumber = arrangka(len(unik))
-array = arrNumber
+panjangunik = len(unik)
+permutasi(panjangunik,ArrPerm)
+for i in range(len(ArrPerm)):
+    isEqual(opperand,result,unik,ArrPerm[i],solusi,countfail)
 
+#print soal
+#for i in range(len(arrAwalv2)):
+#    print(arrAwalv2[i])
+for i in range (len(opperand)):
+    tempstr = str(opperand[i])
+    if(i == len(opperand) -1):
+        print(tempstr.rjust(8),end="+\n")    
+    else:
+        print(tempstr.rjust(8))
+tempstr = str(savesign[0])
+print(tempstr.rjust(8))
+tempstr = str(result[0])
+print(tempstr.rjust(8))
+print("")
+#print solusi
+print ("solusi : \n")
+if(len(solusi) != 0):
+    for ans in range(len(solusi)):
+        tambah = 0
+        anssum = 0
+        for i in range(len(opperand)):
+            for c in range(len(opperand[i])):
+                tambah += solusi[ans][findcidx(opperand[i][c],unik)]*10**(len(opperand[i])-c-1)
+            if i == len(opperand)-1:
+                strnum = str(tambah)
+                print(strnum.rjust(8),end="+\n")
+            else:
+                strnum = str(tambah)
+                print(strnum.rjust(8))
+            tambah = 0
+        tempstr=str(savesign[0])
+        print(tempstr.rjust(8))
+        for c in range(len(result[0])):
+            anssum += solusi[ans][findcidx(result[0][c],unik)]*10**(len(result[0])-c-1)
+        anssnum = str(anssum)
+        print(anssnum.rjust(8))
+        print("")
+else:
+    print("No solution\n")
 
-indexarray = len(unik)
-while indexarray > 0 :
-    for j in range(10-len(unik)):
-        ArrPerm = []
-        for isilist in permutasi(array): 
-            ArrPerm.append(isilist)
-        for i in range(len(ArrPerm)):
-            isEqual(opperand,result,unik,ArrPerm[i],solusi)
-        #ambil element akhir arrayNumber inc
-        print(arrNumber)
-        arrNumber[indexarray-1] += 1
-        print(arrNumber)
-    indexarray -= 1
-
-#dia udah di index akhir
-
-
-
-def permutasii(n):
-    for j in range(10):
-        if  :
-            permutasii(n-1)
-
-            
+print("Number of Tries : ", countfail[0], end=" tries ")
+print("from", len(ArrPerm), "possibility")
+print("Execution time :  %.3f seconds" % (time.time() - start_time))
+input("Press Enter to EXIT. ")
